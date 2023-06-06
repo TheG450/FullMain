45fa3b4b9c39603d9d51a303081645637215415d
TheG450 <120777134+TheG450@users.noreply.github.com>
2023-06-06 23:42:02 +0700

    Update FullPaidScript.lua

--- FullPaidScript.lua
+++ FullPaidScript.lua
@@ -2,7 +2,7 @@ repeat wait(3) until game:IsLoaded()
 local key = _G.Key
 local keys = {"2B4B62-506553-68566D-597133-743677-39GZ", "347437-77217A-25432A-462D4A-614E64-52GZ", "6E5A72-347537-782141-25442A-472D4B-61GZ"}
 if table.find(keys, key) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/USERNAME/REPO/BRANCH/FILE"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheG450/FullMain/main/Paid.lua"))()
 elseif game:GetService("Players").LocalPlayer.Name ~= "GZM450" or game:GetService("Players").LocalPlayer.UserId ~= 322755843 then
     game:GetService("Players").LocalPlayer:Kick("Wrong Player!!!")
 else
