13d6d98566391465a5f8c0a1488198ef7ae020b7
TheG450 <120777134+TheG450@users.noreply.github.com>
2023-06-07 23:14:55 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,7 +1,8 @@
 repeat wait(3) until game:IsLoaded()
 local hwid = {
     ["D9DC421587048D0417A9"] = "D9DC4215-EE09-4AB9-94F8-87048D0417A9",
-	["DF0BDB1ED386F44877D9"] = "DF0BDB1E-FA2C-40BB-A531-D386F44877D9"
+	["DF0BDB1ED386F44877D9"] = "DF0BDB1E-FA2C-40BB-A531-D386F44877D9",
+	["6F2841F1D64CE47F3681"] = "6F2841F1-C8CA-452D-8B72-D64CE47F3681"
 }
 local ClientId = game:GetService("RbxAnalyticsService"):GetClientId()
 local Hwide = string.split((ClientId),'-')
