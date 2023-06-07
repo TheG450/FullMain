0aba54532c4be8583d98f24f3f84acd195aab813
TheG450 <120777134+TheG450@users.noreply.github.com>
2023-06-07 14:50:15 +0700

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,8 +1,8 @@
 repeat wait(3) until game:IsLoaded()
 local key = _G.Key
-local keys = {"2B4B62-506553-68566D-597133-743677-39GZ", 
-    "347437-77217A-25432A-462D4A-614E64-52GZ", 
-    "6E5A72-347537-782141-25442A-472D4B-61GZ"
+local keys = {"2B4B62-506553-68566D-597133-743677-39GZ" == nil, 
+    "347437-77217A-25432A-462D4A-614E64-52GZ" == nil, 
+    "6E5A72-347537-782141-25442A-472D4B-61GZ" == nil
 
 }
 if game:GetService("Players").LocalPlayer.Name == "GZM450" then
